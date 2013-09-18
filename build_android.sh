9c341d071d80824d88d6c266e5ed41a5aa34e0ad
Crossle Song <sxm@yixia.com>
2013-09-18 16:59:30 +0800

    Add android build script

---
+++ build_android.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ -z "$ANDROID_NDK" ]; then
+	echo "You must define ANDROID_NDK before starting."
+	echo "They must point to your NDK directories.\n"
+	exit 1
+fi
+
+SOURCE=`pwd`
+PREFIX=$SOURCE/build/android
+
+SYSROOT=$ANDROID_NDK/platforms/android-14/arch-arm
+CROSS_PREFIX=$ANDROID_NDK/toolchains/arm-linux-androideabi-4.7/prebuilt/linux-x86_64/bin/arm-linux-androideabi-
+
+./configure  --prefix=$PREFIX \
+  --host=arm-linux \
+  --with-sysroot=${SYSROOT} \
+  --disable-shared \
+  --enable-static \
+  --with-pic=no \
+  CC="${CROSS_PREFIX}gcc --sysroot=${SYSROOT}" \
+  CXX="${CROSS_PREFIX}g++ --sysroot=${SYSROOT}" \
+  RANLIB="${CROSS_PREFIX}ranlib" \
+  AR="${CROSS_PREFIX}ar" \
+  AR_FLAGS=rcu \
+  STRIP="${CROSS_PREFIX}strip" \
+  NM="${CROSS_PREFIX}nm" \
+  CFLAGS="-O3  --sysroot=${SYSROOT}" \
+  CXXFLAGS="-O3 --sysroot=${SYSROOT}" \
+  || exit 1
+
+make clean
+make -j4 install || exit 1
+
