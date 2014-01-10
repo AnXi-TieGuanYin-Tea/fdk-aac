eec4e8f668f041a9f43e539d6b8d87d6bf822f48
Crossle Song <sxm@yixia.com>
2014-01-10 13:33:06 +0800

    Support host Linux and Darwin

--- build_android.sh
+++ build_android.sh
@@ -6,11 +6,22 @@ if [ -z "$ANDROID_NDK" ]; then
 	exit 1
 fi
 
+# Detect OS
+OS=`uname`
+HOST_ARCH=`uname -m`
+export CCACHE=; type ccache >/dev/null 2>&1 && export CCACHE=ccache
+if [ $OS == 'Linux' ]; then
+	export HOST_SYSTEM=linux-$HOST_ARCH
+elif [ $OS == 'Darwin' ]; then
+	export HOST_SYSTEM=darwin-$HOST_ARCH
+fi
+
+
 SOURCE=`pwd`
 PREFIX=$SOURCE/build/android
 
 SYSROOT=$ANDROID_NDK/platforms/android-18/arch-arm
-CROSS_PREFIX=$ANDROID_NDK/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86_64/bin/arm-linux-androideabi-
+CROSS_PREFIX=$ANDROID_NDK/toolchains/arm-linux-androideabi-4.8/prebuilt/$HOST_SYSTEM/bin/arm-linux-androideabi-
 
 ./configure  --prefix=$PREFIX \
   --host=arm-linux \
